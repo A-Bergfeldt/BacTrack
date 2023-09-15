c7ead86082f06619964f718bfeb984fc54a41bce
A-Bergfeldt <bergfeldt.andreas@gmail.com>
2023-09-15 14:48:38 +0200

    Removed useless code in users.sql

--- SQL/users.sql
+++ SQL/users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Users (
     user_id VARCHAR(16) PRIMARY KEY,
-    role INT CHECK (role BETWEEN 1 AND 3),
+    role INT CHECK (0<4),
     salt CHAR(3),
     hashed_password VARCHAR(255)
 );
@@ -9,9 +9,3 @@ CREATE TABLE User_roles (
     role_id INT PRIMARY KEY,
     role_name VARCHAR(64)
 );
-
-LOAD DATA INFILE 'user_data_test.txt'
-INTO TABLE Users
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-(user_id, role, salt, hashed_password);
