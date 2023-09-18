5d81a6df7b1270f0f19f77c41c191b340833bd5f
hannamlmv <hanna.malmvall@gmail.com>
2023-09-18 13:29:05 +0200

    Created and populated tables

--- SQL/sample.sql
+++ SQL/sample.sql
@@ -1,18 +1,5 @@
 -- Create Tables
 
--- Create sample table
-CREATE TABLE Sample (
-    sample_id INT PRIMARY KEY AUTO_INCREMENT,
-    date_taken DATE NOT NULL,
-    prescription INT ,
-    status_id INT NOT NULL, 
-    strain INT ,
-    hospital_location INT  NOT NULL,
-    FOREIGN KEY (prescription) REFERENCES antibiotics(antibiotic_id),
-    FOREIGN KEY (status_id) REFERENCES tracking(status_id),
-    FOREIGN KEY (strain) REFERENCES strain(strain_id),
-    FOREIGN KEY (hospital_location) REFERENCES hospital(hospital_id)
-);
 -- Create antibiotic table
 CREATE TABLE Antibiotics (
     antibiotic_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -34,13 +21,44 @@ CREATE TABLE Strain (
 -- Create hospital table
 CREATE TABLE Hospital (
     hospital_id INT PRIMARY KEY AUTO_INCREMENT,
-    hospital_name VARCHAR(50) NOT NULL
+    hospital_name VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    country VARCHAR(50) NOT NULL
+);
+
+-- Create roles table
+CREATE TABLE Roles (
+    role_id INT PRIMARY KEY AUTO_INCREMENT,
+    role_name VARCHAR(64)
+);
+
+-- Create sample table
+CREATE TABLE Sample (
+    sample_id INT PRIMARY KEY AUTO_INCREMENT,
+    date_taken DATE NOT NULL,
+    prescription INT,
+    status_id INT NOT NULL CHECK (0<4), 
+    strain INT,
+    hospital INT NOT NULL,
+    FOREIGN KEY (prescription) REFERENCES Antibiotics(antibiotic_id),
+    FOREIGN KEY (status_id) REFERENCES Tracking(status_id),
+    FOREIGN KEY (strain) REFERENCES Strain(strain_id),
+    FOREIGN KEY (hospital) REFERENCES Hospital(hospital_id)
+);
+
+-- Create user table
+CREATE TABLE Users (
+    user_id VARCHAR(50) PRIMARY KEY,
+    role_id INT NOT NULL CHECK (0<3),
+    salt CHAR(3),
+    hashed_password VARCHAR(255),
+    FOREIGN KEY (role_id) REFERENCES Roles(role_id)
 );
 
 -- Populate tables
 
 -- Populate antibiotics table
-INSERT INTO Antibiotics
+INSERT INTO Antibiotics(antibiotic_name)
 VALUES 
 (
     "Ampicillin"
@@ -50,10 +68,10 @@ VALUES
 ),
 (
     "Erythromycin"
-)
+);
 
 -- Populate tracking table
-INSERT INTO Tracking 
+INSERT INTO Tracking(status_name)
 VALUES
 (
     "Hospital"
@@ -63,10 +81,10 @@ VALUES
 ),
 (
     "Finished"
-)
+);
 
 -- Populate strain table
-INSERT INTO Strain 
+INSERT INTO Strain(strain_name)
 VALUES
 (
     "Escheria coli"
@@ -76,17 +94,47 @@ VALUES
 ),
 (
     "Staphylococcus epidermis"
-)
+);
 
 -- Populate hospital table
-INSERT INTO Hospitals
+INSERT INTO Hospital(hospital_name, city, country)
 VALUES 
 (
-    "Södersjukhuset"
+    "Sahlgrenska sjukhuset", "Göteborg", "Sweden"
 ),
 (
-    "Karolinska sjukhuset"
+    "Karolinska sjukhuset", "Stockholm", "Sweden"
 ),
 (
-    "S:t Görans sjukhus"
-)
+    "Akademiska Sjukhuset", "Uppsala", "Sweden"
+);
+
+-- Populate roles table
+INSERT INTO Roles(role_name)
+VALUES 
+(
+    "Doctor"
+),
+(
+    "Lab worker"
+);
+
+-- Populate user table
+INSERT INTO Users(user_id, role_id, salt, hashed_password)
+VALUES 
+(
+    "Simon_Oscarson","1","123","123"
+),
+(
+    "Andreas_Bergfeldt","2","456","456"
+);
+
+-- Populate sample table
+INSERT INTO Sample(date_taken, prescription, status_id, strain, hospital)
+VALUES 
+(
+    "2023-01-01","1","1","1","1"
+),
+(
+    "2022-06-20","2","2","2","2"
+);
