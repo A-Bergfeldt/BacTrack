317e22b930d9679b9ee47933b37ccb3040cd5f8f
Victor Wong <103962437+vjhwong@users.noreply.github.com>
2023-09-18 11:30:55 +0200

    Added foreign key constraint for Users

--- SQL/users.sql
+++ SQL/users.sql
@@ -2,7 +2,9 @@ CREATE TABLE Users (
     user_id VARCHAR(16) PRIMARY KEY,
     role INT CHECK (0<4),
     salt CHAR(3),
-    hashed_password VARCHAR(255)
+    hashed_password VARCHAR(255),
+    FOREIGN KEY (role) REFERENCES User_roles(role_id)
+
 );
 
 CREATE TABLE User_roles (
