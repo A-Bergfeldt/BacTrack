b7b1cc4055edfa64b5a2f6d7b28d25ab403b49c0
Simonos4 <92506109+Simonos4@users.noreply.github.com>
2023-09-21 09:03:24 +0200

    Create login_w_hash.php

---
+++ login_w_hash.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Verification</title>
+</head>
+<body>
+
+<form method="post">
+    <input type='password' name="password" placeholder="Enter password" />
+    <input type="submit" name="submit" value="Submit" />
+</form>
+
+<?php
+if(isset($_POST["submit"])) {
+    $input = $_POST["password"];
+    $hashedPwdInDb = password_hash("test123", PASSWORD_DEFAULT);
+
+    if (password_verify($input, $hashedPwdInDb)) {
+        echo "Password is correct. You're in!";
+        echo $hashedPwdInDb
+    } else {
+        echo "Password is incorrect. Try again.";
+    }
+}
+?>
+
+</body>
+</html>
