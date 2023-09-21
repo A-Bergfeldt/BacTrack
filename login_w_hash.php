2e7b362fbebbae8028eceedc513a0e0350357bcc
Simonos4 <92506109+Simonos4@users.noreply.github.com>
2023-09-21 11:07:48 +0200

    andreas tur

--- login_w_hash.php
+++ login_w_hash.php
@@ -6,6 +6,7 @@
 <body>
 
 <form method="post">
+    <input type='username' name="username" placeholder="Enter username" />
     <input type='password' name="password" placeholder="Enter password" />
     <input type="submit" name="submit" value="Submit" />
 </form>
@@ -14,24 +15,35 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "bactrack";
+$db_username = "root";
+$db_password = "root";
+$db_name = "bactrack";
+$link = mysqli_connect($servername, $db_username, $db_password, $db_name); 
+
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+    echo "bjhdghsfhkdgshrdhgs";
+  }
 
 if(isset($_POST["submit"])) {
-    $link = mysqli_connect($servername, $username, $password, $dbname); 
 
     $input_username = $_POST["username"];
-    $user_database_password_query = "SELECT hashed_password FROM users WHERE user_id LIKE '%$input_username%';";
+    $user_database_password_query = "SELECT hashed_password FROM users WHERE user_id = $input_username";
     $input_password = $_POST["password"];
     $user_database_password = $link->query($user_database_password_query);
 
+    //echo $input_password;//
+    echo $user_database_password_query;
+    echo $user_database_password;
+    echo password_verify($input_password, $user_database_password);
+
     if (password_verify($input_password, $user_database_password)) {
         echo "Password is correct. You're in!";
-        echo $hashedPwdInDb;
-    } else {
+    } 
+    else {
         echo "Login is incorrect. Try again.";
     }
+
 }
 ?>
 
