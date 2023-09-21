16d32acee0219b1313f4da67627f7f635fc9314a
A-Bergfeldt <bergfeldt.andreas@gmail.com>
2023-09-21 10:05:40 +0200

    Added query for pwd

--- login_w_hash.php
+++ login_w_hash.php
@@ -12,10 +12,19 @@
 
 
 <?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "bactrack";
+
 if(isset($_POST["submit"])) {
+    $link = mysqli_connect($servername, $username, $password, $dbname); 
+
     $input_username = $_POST["username"];
-    $user_database_password = "SELECT `hashed_password` FROM `users` WHERE `user_id`=$input_username;";
+    $user_database_password_query = "SELECT `hashed_password` FROM `users` WHERE `user_id`=$input_username;";
     $input_password = $_POST["password"];
+    $user_database_password = $link->query($user_database_password_query);
 
     if (password_verify($input_password, $user_database_password)) {
         echo "Password is correct. You're in!";
