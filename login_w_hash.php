adaae827f93bd0c677ae9f119f324aa207d16ecc
A-Bergfeldt <bergfeldt.andreas@gmail.com>
2023-09-21 15:08:07 +0200

    nu e de pushat..

--- login_w_hash.php
+++ login_w_hash.php
@@ -20,24 +20,15 @@ $db_password = "root";
 $db_name = "bactrack";
 $link = mysqli_connect($servername, $db_username, $db_password, $db_name); 
 
-if ($link->connect_error) {
-    die("Connection failed: " . $link->connect_error);
-    echo "bjhdghsfhkdgshrdhgs";
-  }
-
 if(isset($_POST["submit"])) {
-
+    
     $input_username = $_POST["username"];
-    $user_database_password_query = "SELECT hashed_password FROM users WHERE user_id = $input_username";
+    $user_database_password_query = "SELECT hashed_password FROM users WHERE user_id = '$input_username'"; // Make SQL query //
     $input_password = $_POST["password"];
     $user_database_password = $link->query($user_database_password_query);
+    $user_database_password_hashed = mysqli_fetch_array($user_database_password); // IMPORTANT: Convert sqli results to array //
 
-    //echo $input_password;//
-    echo $user_database_password_query;
-    echo $user_database_password;
-    echo password_verify($input_password, $user_database_password);
-
-    if (password_verify($input_password, $user_database_password)) {
+    if (password_verify($input_password, $user_database_password_hashed['hashed_password'])) { // IMPORTANT: Index array //
         echo "Password is correct. You're in!";
     } 
     else {
