3f4946d4f470b7ef655205353ec44e41a1e9a325
hannamlmv <hanna.malmvall@gmail.com>
2023-09-22 15:00:02 +0200

    fixed bug about disabled options

--- results_input/disable_options.js
+++ results_input/disable_options.js
@@ -7,21 +7,21 @@ function disable_options(selectElement) {
   var dropdowns = document.getElementsByClassName("antibiotic-dropdown");
 
   var allSelectedValues = [];
-  for (var i = 0; i < dropdowns.length; i++) { 
-    allSelectedValues[i]=dropdowns[i].value;
+  for (var i = 0; i < dropdowns.length; i++) {
+    allSelectedValues[i] = dropdowns[i].value;
   }
 
   // Loop through all the dropdowns
   for (var i = 0; i < dropdowns.length; i++) {
     // Enable all options
     var options = dropdowns[i].getElementsByTagName("option");
-    for (var j = 0; j < options.length; j++) {
+    for (var j = 1; j < options.length; j++) {
       options[j].removeAttribute("disabled");
     }
 
     // Disable the selected option in other dropdowns
     for (var j = 0; j < options.length; j++) {
-      if (allSelectedValues.includes(options[j].value)) {
+      if (allSelectedValues.includes(options[j].value) && options[j].value != dropdowns[i].value) {
         options[j].setAttribute("disabled", "disabled");
       }
     }
