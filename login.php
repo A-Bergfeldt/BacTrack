000b59551d8f5ab8cad43b6b45ed908c8e619dea
Simonos4 <92506109+Simonos4@users.noreply.github.com>
2023-09-22 10:47:03 +0200

    Finalization of the login page

--- login.php
+++ login.php
@@ -55,11 +55,6 @@
 
         <?php
 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
-
             $servername = "localhost";
             $db_username = "root";
             $db_password = "root";
