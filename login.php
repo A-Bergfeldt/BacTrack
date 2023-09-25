9e9452149baf8196a78c2ad94daf8df104729a6d
matildasofia <matilda.styfberg@gmail.com>
2023-09-25 17:26:11 +0200

    Login session is added and user variables stored.
    Login session is initiated. role_id and user_id is also stored in the session.

--- login.php
+++ login.php
@@ -54,6 +54,7 @@
         </form>
 
         <?php
+            session_start();
 
             $servername = "localhost";
             $db_username = "root";
@@ -78,6 +79,13 @@
             
                 if ($user_database_password_hashed !== null && password_verify($input_password, $user_database_password_hashed['hashed_password'])) {
                     // Redirect before sending any content
+                    $stmt = $link->prepare("SELECT role_id FROM users WHERE user_id = ?");
+                    $stmt->bind_param("s", $input_username);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    $user_role = $result->fetch_assoc();
+                    $_SESSION["role_id"] = $user_role;
+                    $_SESSION["user_id"] = $input_username;
                     header("Location: README.txt");
                     exit();
                 } 
