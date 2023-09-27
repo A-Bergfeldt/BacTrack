559ec4c0153f50daa6e07b28012340c41c841faa
hannamlmv <hanna.malmvall@gmail.com>
2023-09-27 17:22:41 +0200

    created a search function
    a search function where you can search for samples by any attribute

--- doctor_view/search_all_samples.php
+++ doctor_view/search_all_samples.php
@@ -1,23 +1,26 @@
+<!-- Create search form -->
+<form method="GET" action="search_all_samples.php">
+  <label for="search">Search for samples: </label>
+  <input type="text" id="search" name="search">
+  <input type="submit" value="Search">
+</form>
+
 <?php
-// I DONT THINK WE NEED THIS MAYBE? ITS MUCH LIKE search_all_samples.php
-
-
 include '../db_connection.php';
 
+// SQL query
+$search = null;
+if (count($_GET) != 0) {
+  $search = $_GET['search'];
+  $sql = "SELECT sample_id, date_taken, status_name, hospital_name, strain_name, doctor_id,lab_technician_id FROM (((sample 
+  INNER JOIN tracking ON sample.status_id = tracking.status_id) 
+  INNER JOIN hospital ON sample.hospital_id = hospital.hospital_id)
+  LEFT JOIN strain ON sample.strain_id = strain.strain_id)
+  WHERE sample_id LIKE '%$search%' or date_taken LIKE '%$search%' or status_name LIKE '%$search%' or hospital_name LIKE '%$search%' or strain_name LIKE '%$search%' or doctor_id LIKE '%$search%' or lab_technician_id LIKE '%$search%'
+  ORDER BY sample_id ASC;";
+  $result = $db_connection->query($sql);
+}
 
-// SQL queries
-$sql = "SELECT sample_id, date_taken, status_name, hospital_name, strain_name, doctor_id,lab_technician_id FROM (((sample 
-INNER JOIN tracking ON sample.status_id = tracking.status_id) 
-INNER JOIN hospital ON sample.hospital_id = hospital.hospital_id)
-LEFT JOIN strain ON sample.strain_id = strain.strain_id)
-ORDER BY sample_id ASC;";
-$result = $db_connection->query($sql);
-
-
-// Close the database connection
-?>
-
-<?php
 // Start the table with some basic styling
 echo '<table style="width: 100%; border-collapse: collapse;">
     <thead>
@@ -33,10 +36,10 @@ echo '<table style="width: 100%; border-collapse: collapse;">
     </thead>
     ';
 // Fill table
-if ($result->num_rows > 0) {
-    echo "<tbody>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>
+if ($search && $result->num_rows > 0) {
+  echo "<tbody>";
+  while ($row = $result->fetch_assoc()) {
+    echo "<tr>
         <td style='border: 1px solid #000; padding: 8px;'>
             <a href='sample_results.php?sample_id=" . $row["sample_id"] . "'>" . $row["sample_id"] . "</a>
         </td>
@@ -47,10 +50,10 @@ if ($result->num_rows > 0) {
         <td style='border: 1px solid #000; padding: 8px;'>" . $row["doctor_id"] . "</td>
         <td style='border: 1px solid #000; padding: 8px;'>" . $row["lab_technician_id"] . "</td>
         </tr>";
-    }
-    echo "</tbody>";
+  }
+  echo "</tbody>";
 } else {
-    echo "<tr>
+  echo "<tr>
     <td colspan='5'>0 results</td>
 </tr>";
 }
