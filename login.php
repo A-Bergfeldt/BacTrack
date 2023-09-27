e6eda286ee6722620cdd5d01670ee1eb4cb2a9cd
matildasofia <matilda.styfberg@gmail.com>
2023-09-27 10:31:36 +0200

    Included "db_connection.php" and removed duplicated code.
    Cleaned up the code by connecting to the database by the "include" statement with the file "db_connection.php" instead of having it in the script.

--- login.php
+++ login.php
@@ -55,16 +55,7 @@
 
         <?php
             session_start();
-
-            $servername = "localhost";
-            $db_username = "root";
-            $db_password = "root";
-            $db_name = "bactrack";
-            $link = mysqli_connect($servername, $db_username, $db_password, $db_name); 
-
-            if (mysqli_connect_error()) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
+            include "db_connection.php";
             
             if(isset($_POST["submit"])) {
                 // Use prepared statements with parameterized queries
