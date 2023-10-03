edede3ba4c8af58d65eef15b1a7804f6c6c6255d
Simonos4 <92506109+Simonos4@users.noreply.github.com>
2023-10-03 11:21:27 +0200

    Added months

--- antibiotic_use_line.php
+++ antibiotic_use_line.php
@@ -11,127 +11,140 @@
     <div id="tester" style="width:600px;height:250px;"></div>
 </div>
 
-    <?php
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
-    require_once 'db_connection.php';
-
-    $query = 
-            "SELECT
-            subquery.sample_year AS years,
-            subquery.ID,
-            antibiotics.antibiotic_name,
-            SUM(subquery.COUNTS) AS count 
-        FROM 
-            (
-                SELECT
-                    r1.antibiotic_id1 AS ID,
-                    COUNT(*) AS COUNTS,
-                    YEAR(sample.date_taken) AS sample_year
-                FROM
-                    results AS r1
-                INNER JOIN
-                    sample ON r1.sample_id = sample.sample_id
-                WHERE
-                    r1.prescribed = 1
-                GROUP BY
-                    r1.antibiotic_id1, YEAR(sample.date_taken)
+require_once 'db_connection.php';
+// Modify your SQL query to retrieve data at a monthly granularity
+$query = 
+    "SELECT
+        subquery.sample_date AS date,
+        subquery.ID,
+        antibiotics.antibiotic_name,
+        COUNT(*) AS count
+    FROM 
+        (
+            SELECT
+                r1.antibiotic_id1 AS ID,
+                sample.date_taken AS sample_date
+            FROM
+                results AS r1
+            INNER JOIN
+                sample ON r1.sample_id = sample.sample_id
+            WHERE
+                r1.prescribed = 1
+                AND NOT r1.antibiotic_id1 = 1
         
-                UNION ALL
+            UNION ALL
         
-                SELECT
-                    r2.antibiotic_id2 AS ID,
-                    COUNT(*) AS COUNTS,
-                    YEAR(sample.date_taken) AS sample_year
-                FROM
-                    results AS r2
-                INNER JOIN
-                    sample ON r2.sample_id = sample.sample_id
-                WHERE
-                    r2.prescribed = 1
-                GROUP BY
-                    r2.antibiotic_id2, YEAR(sample.date_taken)
+            SELECT
+                r2.antibiotic_id2 AS ID,
+                sample.date_taken AS sample_date
+            FROM
+                results AS r2
+            INNER JOIN
+                sample ON r2.sample_id = sample.sample_id
+            WHERE
+                r2.prescribed = 1
+                AND NOT r2.antibiotic_id2 = 1
         
-                UNION ALL
+            UNION ALL
         
-                SELECT
-                    r3.antibiotic_id3 AS ID,
-                    COUNT(*) AS COUNTS,
-                    YEAR(sample.date_taken) AS sample_year
-                FROM
-                    results AS r3
-                INNER JOIN
-                    sample ON r3.sample_id = sample.sample_id
-                WHERE
-                    r3.prescribed = 1
-                GROUP BY
-                    r3.antibiotic_id3, YEAR(sample.date_taken)
-            ) AS subquery
-        INNER JOIN 
-            antibiotics ON antibiotics.antibiotic_id = subquery.ID
-        GROUP BY
-            sample_year,
-            subquery.ID,
-            antibiotics.antibiotic_name
-        ORDER BY
-            antibiotics.antibiotic_name
-            DESC, sample_year;";
-            
-    $result = $db_connection->query($query);
+            SELECT
+                r3.antibiotic_id3 AS ID,
+                sample.date_taken AS sample_date
+            FROM
+                results AS r3
+            INNER JOIN
+                sample ON r3.sample_id = sample.sample_id
+            WHERE
+                r3.prescribed = 1
+                AND NOT r3.antibiotic_id3 = 1
+        ) AS subquery
+    INNER JOIN 
+        antibiotics ON antibiotics.antibiotic_id = subquery.ID
+    GROUP BY
+        subquery.sample_date,
+        subquery.ID,
+        antibiotics.antibiotic_name
+    ORDER BY
+        subquery.sample_date,
+        antibiotics.antibiotic_name";
 
-    $labels  = [];
-    $data = [];
-    $years = [];
+$result = $db_connection->query($query);
 
-    foreach ($result as $row) {
-            $labels[] = $row["antibiotic_name"];
-            $data[] = $row["count"];
-            $years[] = $row["years"];}
+$labels = [];
+$data = [];
+$dates = [];
+
+foreach ($result as $row) {
+    $labels[] = $row["antibiotic_name"];
+    $data[] = $row["count"];
+    $dates[] = $row["date"];
+}
+
+// Organize the data for plotting
+$dataForPlot = [];
+$uniqueLabels = array_unique($labels);
+
+foreach ($uniqueLabels as $label) {
+    $dataForLabel = [];
+    
+    foreach ($dates as $index => $date) {
+        if ($label === $labels[$index]) {
+            $dataForLabel[] = [
+                'x' => $date,
+                'y' => $data[$index]
+            ];
+        }
+    }
+    
+    $dataForPlot[] = [
+        'x' => array_column($dataForLabel, 'x'),
+        'y' => array_column($dataForLabel, 'y'),
+        'type' => 'scatter',
+        'mode' => 'lines',
+        'name' => $label,
+    ];
+}
 
 
-    $n_years = (int)end($years) - (int)$years[0] + 1;
 
-    $year_chunks = array_chunk($years, $n_years);
-    $data_chunks = array_chunk($data, $n_years);
+$labels = json_encode(array_unique($labels));
+?>
 
-    $year_chunks = json_encode($year_chunks);
-    $data_chunks = json_encode($data_chunks);
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Line-plot with Data from SQL Database</title>
+    <script src="https://cdn.plot.ly/plotly-2.26.0.min.js" charset="utf-8"></script>
+</head>
+<body>
+<div class="container">
+    <h2 class="text-center mt-4">Line diagram with Data from SQL Database</h2>
+    <div id="tester" style="width:600px;height:250px;"></div>
+</div>
 
-    $labels = array_unique($labels);
-    $labels = json_encode($labels);
+<script>
+    TESTER = document.getElementById('tester');
+    var data = <?php echo json_encode($dataForPlot); ?>;
 
-    echo $year_chunks;
-    echo $data_chunks;
-    echo $labels;
-
-    ?>
-
-    <script>
-	TESTER = document.getElementById('tester');
-    var xData = <?php echo $year_chunks; ?>;
-    var yData = <?php echo $data_chunks; ?>;
-
-    var data = [];
-
-    for ( var i = 0 ; i < xData.length ; i++ ) {
-    var result = {
-        x: xData[i],
-        y: yData[i],
-        type: 'scatter',
-        mode: 'lines',
+    var layout = {
+        height: 400,
+        width: 500,
+        showlegend: true,
+        xaxis: {
+            title: 'Date'
+        },
+        yaxis: {
+            title: 'Count'
+        }
     };
-    data.push(result);
-};
 
-var layout = {
-  height: 400,
-  width: 500,
-  showlegend: false
-};
-Plotly.newPlot('tester', data, layout);
+    Plotly.newPlot('tester', data, layout);
 </script>
 </body>
 </html>
