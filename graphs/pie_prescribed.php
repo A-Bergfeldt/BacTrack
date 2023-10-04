0b3b7acb1e7340c25d9e7248763a3b8bf1d6558f
Simonos4 <92506109+Simonos4@users.noreply.github.com>
2023-10-04 10:10:39 +0200

    created folder graphs and changed names on the graph files

--- graphs/pie_prescribed.php
+++ graphs/pie_prescribed.php
@@ -9,7 +9,7 @@
 <div class="container">
     <h2 class="text-center mt-4">Pie Chart with Data from SQL Database</h2>
     <div id="year-selector">
-        <form method="get" action="antibiotic_use_pie.php">
+        <form method="get" action="pie_prescribed.php">
             <label for="year">Select a Year:</label>
             <select id="year" name="year">
                 <?php
