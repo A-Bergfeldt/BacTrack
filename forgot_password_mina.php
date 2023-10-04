88afa49ee06026e00564368e9e259a0cde84cd6a
msuhuh <minna.sayehban@gmail.com>
2023-10-04 10:07:35 +0200

    Final changes in forgot password page, should be working

--- forgot_password_mina.php
+++ forgot_password_mina.php
@@ -20,66 +20,16 @@
 
     <div class="loginbox">
         <h1>Forgot password?</h1>
-        <form method="post">
+        <form action="send_reset_link.php" method="POST">
             <div class="txt_field">
                 <input type="text" name="recipient" required>
                 <span></span>
                 <label>Your e-mail</label>
             </div>
+            <input type="hidden" name="subject" value="Password Reset">
+                <input type="hidden" name="message" value="You have requested a password reset. Here is your passwrod:<br>">
             <input type="submit" name="submit" value="Send password">
         </form>
-
-        <?php
-            session_start();
-            include "db_connection.php";
-
-            $servername = "localhost";
-            $db_username = "root";
-            $db_password = "root";
-            $db_name = "bactrack";
-            $link = mysqli_connect($servername, $db_username, $db_password, $db_name); 
-
-            if (mysqli_connect_error()) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
-        
-            if(isset($_POST["submit"])) {
-                // Use prepared statements with parameterized queries
-                $input_username = $_POST["username"];
-                $input_password = $_POST["password"];
-        
-                $stmt = $link->prepare("SELECT hashed_password FROM users WHERE user_id = ?");
-                $stmt->bind_param("s", $input_username);
-                $stmt->execute();
-                $result = $stmt->get_result();
-                $user_database_password_hashed = $result->fetch_assoc();
-        
-                if ($user_database_password_hashed !== null && password_verify($input_password, $user_database_password_hashed['hashed_password'])) {
-                    // Redirect before sending any content
-                    $stmt = $link->prepare("SELECT role_id FROM users WHERE user_id = ?");
-                    $stmt->bind_param("s", $input_username);
-                    $stmt->execute();
-                    $result = $stmt->get_result();
-                    $user_role = $result->fetch_assoc();
-                    $_SESSION["role_id"] = $user_role;
-                    $_SESSION["user_id"] = $input_username;
-                
-                    if ($user_role['role_id'] === 1) {
-                        header("Location: doctor_page.php");
-                    }
-                    if ($user_role['role_id'] === 2) {
-                        header("Location: labtech_page.php");
-                    }
-                
-                    exit();
-                } 
-                else {
-                    // Output error message after the header call
-                    echo "Login failed. Try again.";
-                    exit();
-                }
-            }
-        ?>
     </div>
 </body>
 </html>
