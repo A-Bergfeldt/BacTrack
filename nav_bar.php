816910b72cefef05f0d366347a1114a6819ecd06
A-Bergfeldt <bergfeldt.andreas@gmail.com>
2023-10-04 15:14:44 +0200

    maybe added login/logout

--- nav_bar.php
+++ nav_bar.php
@@ -1,6 +1,6 @@
 <nav>
     <a href="/home/home_page.php">
-        <img src= "/logo_main.png" alt="Logo" width="95" height="65">
+        <img src="/logo_main.png" alt="Logo" width="95" height="65">
     </a>
     <ul>
         <li><a href="/home/home_page.php">Home</a></li>
@@ -20,6 +20,17 @@
             </div>
         </li>
         <li><a href="/statistics/statisitcspage_rough.php">Statistics</a></li>
-        <li><a href="/login/login.php">Login</a></li>
+
+        <?php
+        session_start();
+
+        if(isset($_SESSION['role_id']) && ($_SESSION['role_id'] == 1 || $_SESSION['role_id'] == 2 || $_SESSION['role_id'] == 3)) {
+            echo '<li><a href="/user/my_page.php">My Page</a></li>';
+            echo '<li><a href="/logout/logout.php">Logout</a></li>';
+        } else {
+            echo '<li><a href="/login/login.php">Login</a></li>';
+        }
+        ?>
+
     </ul>
 </nav>
