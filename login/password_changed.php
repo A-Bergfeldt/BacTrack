d278175a7bebb31c9d56bea78037670de4016d88
A-Bergfeldt <bergfeldt.andreas@gmail.com>
2023-10-04 15:38:00 +0200

    changed from html to php

---
+++ login/password_changed.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../nav_bar.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mina Forgot password</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap">
+    <link rel="stylesheet" href="/home/home_style_sheet.css">
+</head>
+
+<body>
+    <div class="loginbox">
+        <h1>Thank you</h1>
+        <h2>Link to change the password is sent to your e-mail</h2>
+        <form action="/login/login.php" method="action">
+            <input type="submit" value="Go to login">
+        </form>
+    </div>
+</body>
+</htm
