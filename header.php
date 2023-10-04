753469546527a202ae3dfc73e7a2071b8114979d
Simonos4 <92506109+Simonos4@users.noreply.github.com>
2023-10-04 14:18:29 +0200

    made header.php

---
+++ header.php
@@ -0,0 +1,25 @@
+<nav>
+    <a href="home_page.php">
+        <img src="logo_main.png" alt="Logo" width="95" height="65">
+    </a>
+    <ul>
+        <li><a href="home_page.php">Home</a></li>
+        <li class="dropdown">
+            <a href="about_page.php" class="dropbtn">About</a>
+            <div class="dropdown-content">
+                <a href="service1.php">About BacTrack</a>
+                <a href="service2.php">About CombiANT</a>
+                <a href="service3.php">About us</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="contact_page.php" class="dropbtn">Contact</a>
+            <div class="dropdown-content">
+                <a href="contactus.php">Contact us</a>
+                <a href="contactus.php">FAQ</a>
+            </div>
+        </li>
+        <li><a href="statisitcspage_rough.php">Statistics</a></li>
+        <li><a href="login.php">Login</a></li>
+    </ul>
+</nav>
