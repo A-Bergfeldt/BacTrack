6ec107700d3535e61b28dacba30d8447a885a71c
corinneolivero <92521444+corinneolivero@users.noreply.github.com>
2023-10-04 11:14:48 +0200

    Renaming the home pages

--- about_page.php
+++ about_page.php
@@ -23,20 +23,20 @@ how much you want
 <head>
     <title>Mina testing home page</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap">
-    <link rel="stylesheet" href="corinne_style_sheet.css">
+    <link rel="stylesheet" href="home_style_sheet.css">
     </head>
 
 <body>
     <nav>
         <!--<div class="logo">Logo here</div>-->
-        <a href="corre_home_page.php">
+        <a href="home_page.php">
         <img src="logo_main.png" alt="Logo" width="95" height="65">
         </a>
         <ul>
-        <li><a href ="corre_home_page.php">Home</a></li>
+        <li><a href ="home_page.php">Home</a></li>
             <li><a href ="about_page.php">About</a></li>
-            <li><a href ="corre_contact_page.php">Contact</a></li>
-            <li><a href ="corre_statistics_page.php">Statistics</a></li>
+            <li><a href ="contact_page.php">Contact</a></li>
+            <li><a href ="statistics_page.php">Statistics</a></li>
             <li><a href ="login.php">Login</a></li>
         </ul>
     </nav>
