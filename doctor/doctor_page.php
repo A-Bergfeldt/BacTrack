61664567925c86c3eb029f4dc157d1adf201559c
matildasofia <matilda.styfberg@gmail.com>
2023-10-05 10:58:55 +0200

    Included nav_bar

--- doctor/doctor_page.php
+++ doctor/doctor_page.php
@@ -23,20 +23,11 @@ how much you want
 <head>
     <title>Mina testing home page</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap">
-    <link rel="stylesheet" href="style_sheet.css">
+    <link rel="stylesheet" href="/login/style_sheet.css">
 </head>
 
 <body>
-    <nav>
-        <div class="logo">Logo here</div>
-        <ul>
-            <li><a href ="home_page.php">Home</a></li>
-            <li><a href ="about_page.php">About</a></li>
-            <li><a href ="contact_page.php">Contact</a></li>
-            <li><a href ="statistics_page.php">Statistics</a></li>
-            <li><a href ="login.php">Login</a></li>
-        </ul>
-    </nav>  
+    <?php require_once "../nav_bar.php"; ?> 
     <div class="container">
         <div class="slides slide1">
             <h1 style="font-size: 100px; color: #fff">Doctor's home page</h1>
