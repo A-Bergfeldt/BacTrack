72d0e2bb6e25b4e29fdbafa91c5f570f133cb377
msuhuh <minna.sayehban@gmail.com>
2023-10-09 14:45:35 +0200

    Trying to update the page

--- data_view/search_all_samples_test.php
+++ data_view/search_all_samples_test.php
@@ -1,7 +1,3 @@
-<!-- Create search form -->
-<script src="multiselect-dropdown.js"></script>
-<link rel="stylesheet" href="table_styles.css">
-
 <?php
 include '../db_connection.php';
 
@@ -30,89 +26,95 @@ $resultStrain = $db_connection->query($sqlStrain);
 $strain = $resultStrain->fetch_all(MYSQLI_ASSOC);
 ?>
 
-
+<br>
+<br>
+<br>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Search all samples</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap">
-    <link rel="stylesheet" href="/data_view/table_stykes.css">
+  <title>Search all samples</title>
+  <script src="multiselect-dropdown.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap">
+  <link rel="stylesheet" href="/data_view/table_styles.css">
+
 </head>
 
 <body>
-    <?php require_once "../nav_bar.php"; ?> 
+  <?php require_once "../nav_bar.php"; ?> 
+  <div class="container">
+    <div class="alter_box">
+      <p>Enter your search</p>
+      <form method="GET" action="search_all_samples.php">
+
+        <label for="search">Sample ID: </label>
+        <select name="search_sample[]" multiple multiselect-search="true" multiselect-select-all="true"
+          multiselect-max-items="1" style="width: 100px;">
+          <?php foreach ($sample as $row): ?>
+            <option value=<?php echo $row['sample_id']; ?>><?php echo $row['sample_id']; ?></option>
+          <?php endforeach ?>
+        </select>
+        <br>
+
+        <label for="search">Status: </label>
+        <select name="search_status[]" multiple multiselect-search="true" multiselect-select-all="true"
+          multiselect-max-items="1" style="width: 100px;">
+          <?php foreach ($status as $row): ?>
+            <option value=<?php echo $row['status_name']; ?>><?php echo $row['status_name']; ?></option>
+          <?php endforeach ?>
+        </select>
+        <br>
+
+        <label for="search">Hospital: </label>
+        <select name="search_hospital[]" multiple multiselect-search="true" multiselect-select-all="true"
+          multiselect-max-items="1" style="width: 150px;">
+          <?php foreach ($hospital as $row): ?>
+            <option value=<?php echo str_replace(" ", "_", $row['hospital_name']); ?>><?php echo $row['hospital_name']; ?>
+            </option>
+          <?php endforeach ?>
+        </select>
+
+        <br>
+
+        <label for="search">Strain: </label>
+        <select name="search_strain[]" multiple multiselect-search="true" multiselect-select-all="true"
+          multiselect-max-items="1" style="width: 100px;">
+          <?php foreach ($strain as $row): ?>
+            <option value=<?php echo str_replace(" ", "_", $row['strain_name']); ?>><?php echo $row['strain_name']; ?></option>
+          <?php endforeach ?>
+        </select>
+        <br>
+
+        <label for="search">Doctor: </label>
+        <select name="search_doctor[]" multiple multiselect-search="true" multiselect-select-all="true"
+          multiselect-max-items="1" style="width: 200px;">
+          <?php foreach ($doctors as $row): ?>
+            <option value=<?php echo str_replace(" ", "_", $row['user_id']); ?>><?php echo $row['user_id']; ?></option>
+          <?php endforeach ?>
+        </select>
+        <br>
+
+        <label for="search">Lab Technician: </label>
+        <select name="search_lab_technician[]" multiple multiselect-search="true" multiselect-select-all="true"
+          multiselect-max-items="1" style="width: 200px;">
+          <?php foreach ($labTechnicians as $row): ?>
+            <option value=<?php echo str_replace(" ", "_", $row['user_id']); ?>><?php echo $row['user_id']; ?></option>
+          <?php endforeach ?>
+        </select>
+
+        <br>
+        <input type="submit" value="Search">
+    </form>
+    </div>
+    <div class="table">
+    
+    </div>
+  </div>
+
 </body>
 </html>
 
 
-
-
-<br>
-<br>
-<br>
-
-<p>Enter your search</p>
-<form method="GET" action="search_all_samples.php">
-
-  <label for="search">Sample ID: </label>
-  <select name="search_sample[]" multiple multiselect-search="true" multiselect-select-all="true"
-    multiselect-max-items="1" style="width: 100px;">
-    <?php foreach ($sample as $row): ?>
-      <option value=<?php echo $row['sample_id']; ?>><?php echo $row['sample_id']; ?></option>
-    <?php endforeach ?>
-  </select>
-  <br>
-
-  <label for="search">Status: </label>
-  <select name="search_status[]" multiple multiselect-search="true" multiselect-select-all="true"
-    multiselect-max-items="1" style="width: 100px;">
-    <?php foreach ($status as $row): ?>
-      <option value=<?php echo $row['status_name']; ?>><?php echo $row['status_name']; ?></option>
-    <?php endforeach ?>
-  </select>
-  <br>
-
-  <label for="search">Hospital: </label>
-  <select name="search_hospital[]" multiple multiselect-search="true" multiselect-select-all="true"
-    multiselect-max-items="1" style="width: 150px;">
-    <?php foreach ($hospital as $row): ?>
-      <option value=<?php echo str_replace(" ", "_", $row['hospital_name']); ?>><?php echo $row['hospital_name']; ?>
-      </option>
-    <?php endforeach ?>
-  </select>
-
-  <br>
-
-  <label for="search">Strain: </label>
-  <select name="search_strain[]" multiple multiselect-search="true" multiselect-select-all="true"
-    multiselect-max-items="1" style="width: 100px;">
-    <?php foreach ($strain as $row): ?>
-      <option value=<?php echo str_replace(" ", "_", $row['strain_name']); ?>><?php echo $row['strain_name']; ?></option>
-    <?php endforeach ?>
-  </select>
-  <br>
-
-  <label for="search">Doctor: </label>
-  <select name="search_doctor[]" multiple multiselect-search="true" multiselect-select-all="true"
-    multiselect-max-items="1" style="width: 200px;">
-    <?php foreach ($doctors as $row): ?>
-      <option value=<?php echo str_replace(" ", "_", $row['user_id']); ?>><?php echo $row['user_id']; ?></option>
-    <?php endforeach ?>
-  </select>
-  <br>
-
-  <label for="search">Lab Technician: </label>
-  <select name="search_lab_technician[]" multiple multiselect-search="true" multiselect-select-all="true"
-    multiselect-max-items="1" style="width: 200px;">
-    <?php foreach ($labTechnicians as $row): ?>
-      <option value=<?php echo str_replace(" ", "_", $row['user_id']); ?>><?php echo $row['user_id']; ?></option>
-    <?php endforeach ?>
-  </select>
-
-  <br>
-  <input type="submit" value="Search">
-</form>
 
 <?php
 
@@ -171,7 +173,7 @@ echo '<div class="scrollable">
     </tr>
     </thead>
     ';
-    
+
 // Fill table
 if (count($_GET) != 0 && $result->num_rows > 0) {
   echo "<tbody>";
