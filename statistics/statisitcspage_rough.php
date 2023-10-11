5924f217e92f60376514dde63e9c7a824d647f00
Simonos4 <92506109+Simonos4@users.noreply.github.com>
2023-10-11 11:31:53 +0200

    Changed text on statistics page

--- statistics/statisitcspage_rough.php
+++ statistics/statisitcspage_rough.php
@@ -13,28 +13,31 @@
             <div class="slides slide2">
                 <!--<h2 style="font-size: 30px; color:black">Services we offer</h2>-->
                 <section class="course">
-                    <h3> Information about visualization provided </h3>
+                    <h3> Analytics from the BacTrack Database </h3>
+                    <p style="font-size: 20px;">BacTrack collects multiple daily entries containing data on prescribed antibiotics for various strains of bacteria. 
+                        Below, you'll find a selection of visualization tools showcasing different types of data. Click on any box to explore further details.</p>
+
                     <div class="row">
 
                         <div class="course-col">
-                            <h3>pie-charts</h3>
-                            <p>Pie charts are a good visualization tool</p>
+                            <h3>Usage</h3>
+                            <p>Pie chart of the antibiotic use on a yearly basis</p>
                             <button id="open">
                             <a href="pie_prescribed.php">Open Page</a>
                             </button>
                         </div>
 
                         <div class="course-col">
-                            <h3>line-graphs</h3>
-                            <p>These are a good way to look for trends</p>
+                            <h3>Trends</h3>
+                            <p>Line chart showing the monthly use of antibiotics over multiple years</p>
                             <button id="open">
                                 <a href="line_prescribed.php">Open Page</a>
                             </button>
                         </div>
 
                         <div class="course-col">
-                            <h3>heatmaps</h3>
-                            <p>One among the best tools to identify trends over a large geographical location</p>
+                            <h3>Outbreaks</h3>
+                            <p>Heatmap of where strains of bacteria were found to spot spread and outbreaks</p>
                             <button id="open">
                                 <a href="heatmap_strain.php">Open Page</a>
                             </button>
