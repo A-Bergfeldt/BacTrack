f4a285043bddd138c1961455770472c5d2ab6889
PraissyZefi <praissy.zefi@gmail.com>
2023-10-11 10:04:32 +0200

    onclick buttons that lead to respective pages

--- statistics/statisitcspage_rough.php
+++ statistics/statisitcspage_rough.php
@@ -10,36 +10,26 @@
     <?php require_once "../nav_bar.php"; ?>
 
         <div class="container">
-            <div class="slides slide2">
+            <div class="slide1">
                 <!--<h2 style="font-size: 30px; color:black">Services we offer</h2>-->
                 <section class="course">
                     <h3> Information about visualization provided </h3>
                     <div class="row">
 
-                        <div class="course-col">
+                        <a class="course-col" href="pie_prescribed.php">
                             <h3>pie-charts</h3>
                             <p>Pie charts are a good visualization tool</p>
-                            <button id="open">
-                            <a href="pie_prescribed.php">Open Page</a>
-                            </button>
-                        </div>
+                        </a>
 
-                        <div class="course-col">
+                        <a class="course-col" href="line_prescribed.php">
                             <h3>line-graphs</h3>
                             <p>These are a good way to look for trends</p>
-                            <button id="open">
-                                <a href="line_prescribed.php">Open Page</a>
-                            </button>
-                        </div>
+                        </a>
 
-                        <div class="course-col">
+                        <a class="course-col" href="heatmap_strain.php">
                             <h3>heatmaps</h3>
                             <p>One among the best tools to identify trends over a large geographical location</p>
-                            <button id="open">
-                                <a href="heatmap_strain.php">Open Page</a>
-                            </button>
-                        </div>
-
+                        </a>
                     </div>      
                 </section>
             </div>
