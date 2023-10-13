2289ce731121e03aa5309d9043e45f296ea3260a
msuhuh <minna.sayehban@gmail.com>
2023-10-13 13:54:46 +0200

    Added table for finished samples

--- doctor/doctor_page.php
+++ doctor/doctor_page.php
@@ -51,7 +51,6 @@ how much you want
 </head>
 
 <body>
-    <?php require_once "../nav_bar.php"; ?> 
     <div class="container">
         <div class="slides slide1">
             <h1 style="font-size: 100px; color: #fff; text-align: center;">Hello <?php echo $user_name; ?>!</h1>
@@ -76,52 +75,42 @@ how much you want
                             <th>Lab Technician<span class="icon-arrow">&DownArrow;</span></th>
                         </tr>
                     </thead>
-                <tbody>
-            <?php
-            $dropdownNames = array(
-              "search_sample" => "sample_id",
-              "search_status" => "status_name",
-              "search_hospital" => "hospital_name",
-              "search_strain" => "strain_name",
-              "search_doctor" => "doctor_id",
-              "search_lab_technician" => "lab_technician_id"
-            );
+                    <tbody>
+                        <?php
+                        if (count($_GET) != 0) {
+                            $sql = "SELECT sample_id, date_taken, status_name, hospital_name, strain_name, doctor_id, lab_technician_id FROM (((sample 
+                            INNER JOIN tracking ON sample.status_id = tracking.status_id) 
+                            INNER JOIN hospital ON sample.hospital_id = hospital.hospital_id)
+                            LEFT JOIN strain ON sample.strain_id = strain.strain_id) "
+                                . $whereStatements .
+                                " ORDER BY sample_id ASC;";
+                            $result = $db_connection->query($sql);
 
-            if (count($_GET) != 0) {
-                $sql = "SELECT sample_id, date_taken, status_name, hospital_name, strain_name, doctor_id, lab_technician_id FROM (((sample 
-                INNER JOIN tracking ON sample.status_id = tracking.status_id) 
-                INNER JOIN hospital ON sample.hospital_id = hospital.hospital_id)
-                LEFT JOIN strain ON sample.strain_id = strain.strain_id) "
-                    . $whereStatements .
-                    " ORDER BY sample_id ASC;";
-                $result = $db_connection->query($sql);
+                            while ($row = $result->fetch_assoc()) {
+                                echo "<tr>
+                                    <td>
+                                        <a href='view_finished_samples.php?sample_id=" . $row["sample_id"] . "'>" . $row["sample_id"] . "</a>
+                                    </td>
+                                    <td>" . $row["date_taken"] . "</td>
+                                    <td>" . $row["strain_name"] . "</td>
+                                    <td>" . $row["lab_technician_id"] . "</td>
+                                </tr>";
+                            }
 
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr>
-                        <td>
-                            <a href='view_finished_samples.php?sample_id=" . $row["sample_id"] . "'>" . $row["sample_id"] . "</a>
-                        </td>
-                        <td>" . $row["date_taken"] . "</td>
-                        <td>" . $row["strain_name"] . "</td>
-                        <td>" . $row["lab_technician_id"] . "</td>
-                    </tr>";
-                }
-
-                $result->free_result();
-                $db_connection->close();
-            } 
-            
-            else {
-                echo "<tr><td colspan='7'>0 results</td></tr>";
-            }
-            ?>
-          </tbody>
-        </table>
-      </section>
-    </main>
-
-
+                            $result->free_result();
+                            $db_connection->close();
+                        } 
+                        
+                        else {
+                            echo "<tr><td colspan='7'>0 results</td></tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </section>
+        </main>
     </div>
+    <?php require_once "../nav_bar.php"; ?> 
 </body>
 </html>
 
