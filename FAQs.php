e113f755b8757ffe874f2667bdc6b4c0540f0b5b
Sena15 <129276583+Sena15@users.noreply.github.com>
2023-10-13 12:52:36 +0200

    changes

--- FAQs.php
+++ FAQs.php
@@ -6,91 +6,138 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="contactus.css">
     <title>Frequently asked questions - BacTrack</title>
+    <style>
+        .faq {
+            text-align: center; /* Center all content */
+        }
+
+        .faq-question {
+            font-weight: bold;
+            font-size: 24px;
+            cursor: pointer;
+            margin-bottom: 10px;
+            text-align: left; /* Align questions to the left */
+        }
+
+        .faq-answer {
+            display: none;
+            margin-bottom: 10px;
+            font-size: 18px;
+            text-align: left; /* Align answers to the left */
+        }
+    </style>
 </head>
 
 <body>
     <h1 class="centered-paragraph">Frequently Asked Questions</h1>
-
     <div class="faq">
-
-        <div class="question-answer">
-            <h3>1. What is BacTrack?</h3>
-            <p>BacTrack is a healthcare project dedicated to tracking antibiotic combination therapies for improved
+        <div class="faq-item">
+            <div class="faq-question">
+                <span class="faq-toggle"></span> 1. What is BacTrack?
+            </div>
+            <div class="faq-answer">
+                BacTrack is a healthcare project dedicated to tracking antibiotic combination therapies for improved
                 healthcare outcomes.
-            </p>
+            </div>
         </div>
 
-        <div class="question-answer">
-            <h3>2. How does BacTrack work?</h3>
-            <p>BacTrack utilizes advanced data analysis techniques to monitor and analyze the effectiveness of various
+        <div class="faq-item">
+            <div class="faq-question">
+                <span class="faq-toggle"></span> 2. How does BacTrack work?
+            </div>
+            <div class="faq-answer">
+                BacTrack utilizes advanced data analysis techniques to monitor and analyze the effectiveness of various
                 antibiotic combinations in treating medical conditions.
-            </p>
+            </div>
         </div>
 
-        <div class="question-answer">
-            <h3>3. Who can benefit from BacTrack?</h3>
-            <p>BacTrack can benefit healthcare professionals, researchers, and patients involved in antibiotic
+        <div class="faq-item">
+            <div class="faq-question">
+                <span class="faq-toggle"></span> 3. Who can benefit from BacTrack?
+            </div>
+            <div class="faq-answer">
+                BacTrack can benefit healthcare professionals, researchers, and patients involved in antibiotic
                 therapies and healthcare analysis.
-            </p>
+            </div>
         </div>
 
-        <div class="question-answer">
-            <h3>4. Is BacTrack compliant with privacy regulations?</h3>
-            <p>Yes, BacTrack prioritizes data privacy and complies with all relevant healthcare data protection
+        <div class="faq-item">
+            <div class="faq-question">
+                <span class="faq-toggle"></span> 4. Is BacTrack compliant with privacy regulations?
+            </div>
+            <div class="faq-answer">
+                Yes, BacTrack prioritizes data privacy and complies with all relevant healthcare data protection
                 regulations.
-            </p>
+            </div>
         </div>
 
-        <div class="question-answer">
-            <h3>5. How can I get started with BacTrack?</h3>
-            <p>To get started with BacTrack, simply sign up for an account on our website and follow the provided
+        <div class="faq-item">
+            <div class="faq-question">
+                <span class="faq-toggle"></span> 5. How can I get started with BacTrack?
+            </div>
+            <div class="faq-answer">
+                To get started with BacTrack, simply sign up for an account on our website and follow the provided
                 instructions to access our tracking and analysis tools.
-            </p>
+            </div>
         </div>
 
-        <div class="question-answer">
-            <h3>6. Are there any fees associated with using BacTrack?</h3>
-            <p>
+        <div class="faq-item">
+            <div class="faq-question">
+                <span class="faq-toggle"></span> 6. Are there any fees associated with using BacTrack?
+            </div>
+            <div class="faq-answer">
                 Yes, we do have fees associated with using BacTrack. We charge for the services provided.
-            </p>
+            </div>
         </div>
 
-        <div class="question-answer">
-            <h3>7. Can I collaborate with other healthcare professionals on BacTrack?</h3>
-            <p>Yes, BacTrack offers collaboration features that allow healthcare professionals to work together on
+        <div class="faq-item">
+            <div class="faq-question">
+                <span class="faq-toggle"></span> 7. Can I collaborate with other healthcare professionals on BacTrack?
+            </div>
+            <div class="faq-answer">
+                Yes, BacTrack offers collaboration features that allow healthcare professionals to work together on
                 tracking and analyzing antibiotic combination therapies.
-            </p>
+            </div>
         </div>
 
-        <div class="question-answer">
-            <h3>8. How can I contact BacTrack support?</h3>
-            <p>For any support or inquiries, please reach out to our support team at <a href="contactus.php">Contact Us</a></p>
+        <div class="faq-item">
+            <div class="faq-question">
+                <span class="faq-toggle"></span> 8. How can I contact BacTrack support?
+            </div>
+            <div class="faq-answer">
+                For any support or inquiries, please reach out to our support team at <a href="contactus.php">Contact
+                    Us</a>
+            </div>
         </div>
 
-        <div class="question-answer">
-            <h3>9. Is my data secure with BacTrack?</h3>
-            <p>
+        <div class="faq-item">
+            <div class="faq-question">
+                <span class="faq-toggle"></span> 9. Is my data secure with BacTrack?
+            </div>
+            <div class="faq-answer">
                 Yes, BacTrack employs security measures to ensure the safety and confidentiality of your data.
-            </p>
+            </div>
         </div>
 
-        <div class="question-answer" class="spacer">
-            <h3>10. Can I export my tracked data from BacTrack?</h3>
-            <p>Yes, BacTrack provides options to export your tracked data in various formats for further analysis or
+        <div class="faq-item">
+            <div class="faq-question">
+                <span class="faq-toggle"></span> 10. Can I export my tracked data from BacTrack?
+            </div>
+            <div class="faq-answer">
+                Yes, BacTrack provides options to export your tracked data in various formats for further analysis or
                 documentation.
-            </p>
+            </div>
         </div>
 
     </div>
 
     <script>
-        document.querySelectorAll('.dropdown-toggle').forEach(button => {
-            button.addEventListener('click', () => {
-                const content = button.nextElementSibling;
-                content.style.display = content.style.display === 'block' ? 'none' : 'block';
+        document.querySelectorAll('.faq-question').forEach(function (item) {
+            item.addEventListener('click', function () {
+                var answer = this.nextElementSibling;
+                answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
             });
         });
     </script>
-
 
 </body>
--- privacy_policy.php
+++ privacy_policy.php
@@ -15,14 +15,21 @@
         function showPolicy() {
             var selectedSection = document.getElementById("policy-dropdown").value;
             var allSections = document.getElementsByClassName("policy-content");
-            for (var i = 0; i < allSections.length; i++) {
-                allSections[i].style.display = "none";
+
+            if (selectedSection === "0") {
+                for (var i = 0; i < allSections.length; i++) {
+                    allSections[i].style.display = "block";
+                }
+            } else {
+                for (var i = 0; i < allSections.length; i++) {
+                    allSections[i].style.display = "none";
+                }
+                document.getElementById("policy-content-" + selectedSection).style.display = "block";
             }
-            document.getElementById("policy-content-" + selectedSection).style.display = "block";
         }
     </script>
 
-<div class="Privacy-policy">
+    <div class="Privacy-policy">
         <select id="policy-dropdown" onchange="showPolicy()">
             <option value="0">Select a section</option>
             <option value="1">Information We Collect</option>
@@ -38,7 +45,7 @@
     <div id="policy-content-1" class="policy-content">
         <h2>1. Information We Collect</h2>
         <p>
-            <h3>Personal Information</h3>
+        <h3>Personal Information</h3>
         </p>
         <p> Name: To provide personalized services and user identification.</p>
         <p> Contact Information: Such as email address for communication purposes.</p>
@@ -53,44 +60,59 @@
     <div id="policy-content-3" class="policy-content">
         <h2>3. How do we Use Your Information</h2>
         <p><b>We use the information we collect for the following purposes:</b></p>
-        <p> Providing Services: To track and analyze antibiotic combination therapies for better healthcare outcomes.</p>
+        <p> Providing Services: To track and analyze antibiotic combination therapies for better healthcare outcomes.
+        </p>
         <p> Communication: To send relevant updates, notifications, and respond to inquiries.</p>
     </div>
 
     <div id="policy-content-4" class="policy-content">
         <h2>4. How do we store your data</h2>
-        <p>Our company securely stores your data on a local computer, and the responsibility for managing this data rests with our designated administrator.</p>
-        <p>Our company retains your data for a duration of four months. After this period concludes, we ensure complete removal by erasing it from our database.</p>
-        
+        <p>Our company securely stores your data on a local computer, and the responsibility for managing this data
+            rests with our designated administrator.</p>
+        <p>Our company retains your data for a duration of four months. After this period concludes, we ensure complete
+            removal by erasing it from our database.</p>
+
     </div>
 
     <div id="policy-content-5" class="policy-content">
         <h2>5. Your Choices and Rights</h2>
         <p><b>You have the right to:</b></p>
-        <p> Access, Correct, or Delete Your Information: You can request access, correction, or deletion of your personal information by contacting us.</p>
+        <p> Access, Correct, or Delete Your Information: You can request access, correction, or deletion of your
+            personal information by contacting us.</p>
         <p> Opt-Out of Communications: You can opt-out of receiving promotional communications from us at any time.</p>
-        <p> The right to rectification: You have the right to request that Our Company amend any information you believe to be inaccurate. Additionally, you have the right to request Our Company to supplement any information you believe to be incomplete.</p>
+        <p> The right to rectification: You have the right to request that Our Company amend any information you believe
+            to be inaccurate. Additionally, you have the right to request Our Company to supplement any information you
+            believe to be incomplete.</p>
     </div>
 
     <div id="policy-content-6" class="policy-content">
         <h2>6. Cookies</h2>
-        <p>Cookies are text files placed on your computer to collect standard Internet log information and visitor behavior information. When you visit our websites, we may collect information from you automatically through cookies or similar technology.</p>
-        
+        <p>Cookies are text files placed on your computer to collect standard Internet log information and visitor
+            behavior information. When you visit our websites, we may collect information from you automatically through
+            cookies or similar technology.</p>
+
         <h3><b>6.1. How do we use cookies</b></h3>
         <p>Our Company utilizes cookies for various purposes to enhance your website experience, such as:</p>
         <li>Maintaining your signed-in status.</li>
 
         <h3><b>6.2. What type of cookies do we use</b></h3>
-        <p>Session cookies: At BacTrack, we implement session cookies to elevate your browsing experience. These cookies are temporary and remain on your device solely for the duration of your current session. When you conclude your browsing session, please be aware that they will persist until manually deleted by you, rather than being automatically removed.</p>
-    
+        <p>Session cookies: At BacTrack, we implement session cookies to elevate your browsing experience. These cookies
+            are temporary and remain on your device solely for the duration of your current session. When you conclude
+            your browsing session, please be aware that they will persist until manually deleted by you, rather than
+            being automatically removed.</p>
+
         <h3><b>6.3. How to manage cookies</b></h3>
-        <p>You have the option to configure your browser to decline cookies, and the aforementioned website provides instructions on how to delete them from your browser. It's important to note that in a few instances, disabling cookies may impact the functionality of certain features on our website.</p>
+        <p>You have the option to configure your browser to decline cookies, and the aforementioned website provides
+            instructions on how to delete them from your browser. It's important to note that in a few instances,
+            disabling cookies may impact the functionality of certain features on our website.</p>
     </div>
 
     <div id="policy-content-7" class="policy-content">
         <h2>7. Changes to this policy</h2>
-        <p>We may update this Privacy Policy periodically to reflect changes in our practices or for legal compliance. You will be notified of any significant changes.</p>
-        <p>If you have any questions, concerns, or requests regarding this Privacy Policy or our data practices, please contact us at: <a href="contactus.php">here</a></p>
+        <p>We may update this Privacy Policy periodically to reflect changes in our practices or for legal compliance.
+            You will be notified of any significant changes.</p>
+        <p>If you have any questions, concerns, or requests regarding this Privacy Policy or our data practices, please
+            contact us at: <a href="contactus.php">here</a></p>
     </div>
 
 </body>
